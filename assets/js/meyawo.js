13ae969b34a33541a056805e9a6de9ee5642feb5
Prithiviraj Rathinam <pritz94@gmail.com>
2025-02-27 15:07:39 +0530

    update some content

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -36,3 +36,38 @@ $('#nav-toggle').click(function(){
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const comment = document.getElementById('comment').value;
+
+    const data = {
+        name: name,
+        email: email,
+        comment: comment
+    };
+
+    fetch('https://api.web3forms.com/submit', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+        access_key: '3b42b856-2470-4b14-9292-ce3449888998',
+        ...data
+        })
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Form submitted successfully!');
+        } else {
+            alert('There was an error submitting the form.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('There was an error submitting the form.');
+    });
+}
